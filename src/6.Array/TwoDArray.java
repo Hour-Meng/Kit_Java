c84a6b75108d0ba9d2e230008e07078716e1da64
Hour-Meng <hourmeng.eang26@kit.edu.kh>
2026-06-22 10:56:56 +0700

    22 june 2026

--- src/6.Array/TwoDArray.java
+++ src/6.Array/TwoDArray.java
@@ -4,6 +4,7 @@ public class TwoDArray {
         
         // Create the 2d array
 
+                                // this is a 2 by 2 array. It does not count like index which start from 0
         int [][] matrix = new int[2][2];
 
         matrix = new int[][]{{1,2}, {1,3}};
@@ -15,8 +16,12 @@ public class TwoDArray {
         for( int i = 0; i < matrix.length ; i++){
 
             for (int j = 0; j < matrix[i].length; j++){
-                System.out.println(matrix[i][j]);
+                System.out.print(matrix[i][j] + " ");
             }
+
+            System.out.println();
+
+            // This will print out a matrix style output
         }
 
     }
